e8adb49db64a1d48664ebb95c4c42ff50f0d7d16
zsomborjoel <zsomborjoel@gmail.com>
2020-09-19 13:36:40 +0200

    Expenses page dode

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Expense-Tracker
+
+* SpringBoot
+* H2
+* React js
+
+![Expenses page](https://github.com/zsomborjoel/Expense-Tracker/blob/master/homeexpenses_expenses_screen.PNG)
--- homeexpense/app/src/Expenses.js
+++ homeexpense/app/src/Expenses.js
@@ -2,31 +2,104 @@ import React, { Component } from 'react';
 import AppNav from './AppNav';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-import { Button, Container, FormGroup, Form, Label, Input } from 'reactstrap';
+import { Button, Container, FormGroup, Form, Label, Input, Table } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
 
 class Expense extends Component {
 
-    state = { 
-        date: new Date(),
-        isLoading: true,
-        expenses: [],
-        categorires: []
+    // read only - passed to component
+    // automaticaly binds after submit (callback need to be bind)
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            date: new Date(),
+            isLoading: true,
+            item: this.emptyItem,
+            Categorires: [],
+            Expenses: []
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+    }
+
+    // default send item construct
+    emptyItem = {
+        id: '100',
+        expenseDate: new Date(),
+        description: '',
+        location: '',
+        category: {id:1, name:'Travel'}
+    }
+
+    // based on events
+    // prevent auto submittion
+    async handleSubmit(event) {
+        event.preventDefault();
+        const {item} = this.state;
+        await fetch('/api/expenses', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        });
+        console.log(this.state);
+        this.props.history.push("/expenses")
+    }
+
+    async handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.item};
+        item[name] = value;
+        this.setState({item});
+
+        console.log(this.state);
+    }
+
+    async handleDateChange(date) {
+        let item = {...this.state.item};
+        item.expenseDate = date;
+        this.setState({item});
+    }
+
+    async remove(id) {
+        await fetch(`/api/expenses/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            // get from state and filter on id
+            let updatedExpenses = [...this.state.Expenses].filter(i => i.id !== id);
+            this.setState({Expenses: updatedExpenses});
+        });
     }
 
     // await lets you it fetched data and it is ready
     // always modify state with setState
     async componentDidMount() {
-        const response = await fetch('/api/categories');
-        const body = await response.json();
+        const responsCategories = await fetch('/api/categories');
+        const bodyCategories = await responsCategories.json();
+        this.setState({Categorires: bodyCategories, isLoading: false});
 
-        this.setState({Categorires: body, isLoading: false});
+        const responseExpenses = await fetch('/api/expenses');
+        const bodyExpenses = await responseExpenses.json();
+        this.setState({Expenses: bodyExpenses, isLoading: false});        
     }
 
+    // isLoading to last to wait for both calls
     render() { 
         const title = <h3>Add Expense</h3>;
-        const {Categorires, isLoading} = this.state;
-
+        const {Categorires} = this.state;
+        const {Expenses, isLoading} = this.state;
 
         if (isLoading)
             return<div>Loading...</div>
@@ -36,6 +109,17 @@ class Expense extends Component {
                 <option value={category.id} key={category.id}>
                     {category.name}
                 </option>
+            )
+
+        let rows =
+            Expenses.map( expense => 
+                <tr key={expense.id}>
+                    <td>{expense.description}</td>
+                    <td>{expense.place}</td>
+                    <td><Moment date={expense.expenseDate} format="YYYY/MM/DD"/></td>
+                    <td>{expense.category.name}</td>
+                    <td><Button size="sm" color="danger" onClick={ () => this.remove(expense.id)}>Delete</Button></td>
+                </tr>
             )
     
         return (
@@ -55,8 +139,6 @@ class Expense extends Component {
                             <select>
                                 {optionList}
                             </select>
-
-                            <Input type="text" name="category" id="category" onChange={this.handleChange}/>
                         </FormGroup>
 
                         <FormGroup>
@@ -81,6 +163,25 @@ class Expense extends Component {
                             <Button color="secondary" tag={Link} to="/">Cancel</Button>
                         </FormGroup>
                     </Form>
+                </Container>
+
+                <Container>
+                    <h3>Expense list</h3>
+                    <Table className="mt-4">
+                        <thead>
+                            <tr>
+                                <th with="20%">Description</th>
+                                <th with="10%">Location</th>
+                                <th>Date</th>
+                                <th>Category</th>
+                                <th with="10%">Action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {rows}
+                        </tbody>
+                    </Table>
                 </Container>
             </div>   
         );
